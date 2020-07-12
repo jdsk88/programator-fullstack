7c6a67b5a255a458fd3743a2483ea08990fbc799
Your Name <you@example.com>
2020-07-12 15:10:26 +0200

    Tassks

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,8 +6,28 @@ routes.get("/", (req, res) => {
   res.send("Hello API!");
 });
 
+// routes.use('/products')
 routes.get("/products", (req, res) => {
   res.send(["produkt 1"]);
 });
+
+// Aneta
+// routes.use('/users')
+
+// Jakub
+// routes.use('/comments')
+
+// Damian
+// routes.use('/categories')
+
+// Kasia
+// routes.use('/orders')
+
+// Maciej
+// routes.use('/cart')
+
+// Liumila
+// routes.use('/wishlist')
+
 
 export default routes;
