3b7f5e8e745905fefc55a80105f63fe73433dda3
MJ <jdsk88@gmail.com>
2020-07-12 16:09:44 +0200

    os.networkInterfaces addes

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -6,6 +6,14 @@ routes.get("/", (req,res) => {
     res.send(["cart data"]);
 })
 
+routes.get("/saved", (req,res) => {
+    res.send(["save data"]);
+})
+
+routes.get("/saved", (req,res) => {
+    res.send(["saved data"]);
+})
+
 
 
 export default routes 
