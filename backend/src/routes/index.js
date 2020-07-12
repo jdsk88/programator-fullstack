a9bd702d54ab87f9be298f4299dd10bdd3918e79
Your Name <you@example.com>
2020-07-12 14:52:21 +0200

    node api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 
 const routes = express.Router({});
 
+routes.get("/", (req, res) => {
+  res.send("Hello API!");
+});
 
-routes.get('/products',(req,res)=>{
-  res.send([
-    'produkt 1'
-  ])
-})
+routes.get("/products", (req, res) => {
+  res.send(["produkt 1"]);
+});
 
 export default routes;
